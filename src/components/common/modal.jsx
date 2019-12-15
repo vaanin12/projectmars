58e8d8aa72dc4a59bf610f3da1963fc997a46923
vaanin12 <58896971+vaanin12@users.noreply.github.com>
2019-12-15 18:53:36 +0530

    Update modal.jsx

--- src/components/common/modal.jsx
+++ src/components/common/modal.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Modal = ({ data, handleSubmit, openModal, closeModal }) => {
-	const [input, setInput] = useState();
-	if(!openModal) return null;
+  const [input, setInput] = useState();
+  if (!openModal) return null;
 
-	return (
-		<div className="card text-white bg-primary mb-3" style={{maxWidth: "20rem"}}>
-		  <div className="card-header">{ data.name }</div>
-		  <div className="card-body">
-		    <form onSubmit={(event) => handleSubmit()}>
-	    		<div class="form-group">
-				  <input 
-				  	type="text" 
-				  	class="form-control"
-				  	value={data.budget} 
-				  	onChange={(e) => setInput(e.target.value)} 
-				  />
-				</div>
-				<button 
-					type="button" 
-					class="btn btn-secondary">
-					update
-				</button>
-				<button 
-					type="button"
-					onClick={(e) => closeModal()}
-					class="btn btn-danger mx-2">
-					close
-				</button>
-		    </form>
-		  </div>
-		</div>
-	)
-}
+  return (
+    <div
+      className="card text-white bg-primary mb-3"
+      style={{ maxWidth: "20rem" }}
+    >
+      <div className="card-header">{data.name}</div>
+      <div className="card-body">
+        <form onSubmit={event => handleSubmit(event)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              value={data.budget}
+              onChange={e => setInput(e.target.value)}
+            />
+          </div>
+          <button type="button" className="btn btn-secondary">
+            update
+          </button>
+          <button
+            type="button"
+            onClick={e => closeModal()}
+            className="btn btn-danger mx-2"
+          >
+            close
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Modal;
